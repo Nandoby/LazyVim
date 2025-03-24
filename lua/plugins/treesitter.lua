085c60e77919835fe1ca3aeecb66cb7a7f4a89ef
Nando Biaccalli <nandobiaccalli@gmail.com>
2025-03-24 22:18:14 +0100

    feat(treesitter): don't use auto_install. Use `ensure_installed` instead

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,26 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = "BufReadPost",
-    config = function ()
+    config = function()
       require("nvim-treesitter.configs").setup({
         sync_install = false,
-        auto_install = true,
+        ensure_installed = {
+          "bash",
+          "help",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "regex",
+          "tsx",
+          "typescript",
+          "vim",
+          "yaml",
+        },
         highlight = { enable = true },
         indent = { enable = true },
         context_commentstring = { enable = true, enable_autocmd = false },
