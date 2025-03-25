5f6c008817b33a56521c0095e723d129765690cd
Nando Biaccalli <nandobiaccalli@gmail.com>
2025-03-25 22:10:15 +0100

    feat(bufferline): added diagnostic icons

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -24,7 +24,7 @@ return {
         return vim.ui.select(...)
       end
       ---@diagnostic disable-next-line:duplicate-set-field
-      vim.ui.input = function (...)
+      vim.ui.input = function(...)
         require("lazy").load({ plugins = { "dressing.nvim" } })
         return vim.ui.input(...)
       end
@@ -35,7 +35,26 @@ return {
   {
     "akinsho/nvim-bufferline.lua",
     event = "BufAdd",
-    config = true,
+    config = {
+      options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("lazyvim.config.icons").diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
   },
 
   -- statusline
