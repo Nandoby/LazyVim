228b442008365b552a3b8f346fc593569badbc87
Nando Biaccalli <nandobiaccalli@gmail.com>
2025-04-02 21:59:17 +0200

    fix(core): use lazy.nvim version configured by the user

--- lua/lazyvim/plugins/core.lua
+++ lua/lazyvim/plugins/core.lua
@@ -1,13 +1,9 @@
-local specs = {
-  -- set to HEAD for now. i'm still making too many changes for this repo related to lazy itself
-  { "folke/lazy.nvim", version = false },
-}
-
--- only add for >=9.0.1, since there's an endless loop in earlier versions
-if require("lazyvim.config").has(">=9.1.0") then
-  specs[#specs + 1] = { "LazyVim/LazyVim", priority = 10000, lazy = false, config = true }
-else
+-- only add for >= 9.0.1, since there's an endless loop in earlier versions
+if not require("lazyvim.config").has(">=9.1.0") then
   require("lazyvim.config").setup()
+  return {}
 end
 
-return specs
+return {
+  { "LazyVim/LazyVim", priority = 10000, lazy = false, config = true },
+}
